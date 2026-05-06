37cfdbe7b34e7353db593b3ea3bdbf848681e98c
Юлиан Младенов / Yulian Mladenov <yulian.mladenovv@gmail.com>
2026-05-06 16:52:08 +0200

    Add input validation and play again loop

--- Project Rock-Paper-Scissors/Rock-Paper-Scissor.py
+++ Project Rock-Paper-Scissors/Rock-Paper-Scissor.py
@@ -1,36 +1,53 @@
-import random
+while True:
+    import random
 
-player_input = input("Choose [r]ock, [p]aper, [s]cissors: ")
+    player_input = input("Choose [r]ock, [p]aper, [s]cissors: ")
 
-rock = 'Rock'
-paper = 'Paper'
-scissors = 'Scissors'
-player_move = ''
+    rock = 'Rock'
+    paper = 'Paper'
+    scissors = 'Scissors'
+    player_move = ''
 
-if player_input == 'r':
-    player_move = rock
-elif player_input == 'p':
-    player_move = paper
-elif player_input == 's':
-    player_move = scissors
-else:
-    raise SystemExit('Invalid input. Try again...')
-computer_random_number = random.randint(1, 3)
+    if player_input == 'r':
+        player_move = rock
+    elif player_input == 'p':
+        player_move = paper
+    elif player_input == 's':
+        player_move = scissors
+    else:
+        while True:
+            if player_input != 'r' and player_input != 's' and player_input != 'p':
+                player_input = input("Your input is wrong! Please choose again only one of the next symbols: [r]ock, [p]aper, [s]cissors: ")
+            else:
+                player_move = player_input
+                break
 
-computer_move = ''
+    computer_random_number = random.randint(1, 3)
 
-if computer_random_number == 1:
-    computer_move = rock
-elif computer_random_number == 2:
-    computer_move = paper
-elif computer_random_number == 3:
-    computer_move = scissors
+    computer_move = ''
 
-# switch cases
-if (player_move == rock and computer_move == scissors) or (player_move == paper and computer_move == rock) or \
-        (player_move == scissors and computer_move == paper):
-    print('You win!')
-elif player_move == computer_move:
-    print('Draw!')
-else:
-    print('You lose!')
+    if computer_random_number == 1:
+        computer_move = rock
+    elif computer_random_number == 2:
+        computer_move = paper
+    elif computer_random_number == 3:
+        computer_move = scissors
+
+    # switch cases
+    if (player_move == rock and computer_move == scissors) or (player_move == paper and computer_move == rock) or \
+            (player_move == scissors and computer_move == paper):
+        print('You win!')
+    elif player_move == computer_move:
+        print('Draw!')
+    else:
+        print('You lose!')
+
+    while True:
+        play_again = input("Play again? [yes/no]: ")
+        if play_again == 'yes':
+            break
+        elif play_again == 'no':
+            raise SystemExit('Thank you for playing!')
+        else:
+            print("Your input is wrong. If You want to play again, please write 'yes' or 'no'")
+
